bc8efb943d20e90a6429e09a648dfaa05116c39f
KrishnaPriyankaD <KrishnaPriyanka.Dachepalli@cognizant.com>
2026-02-04 19:02:20 +0530

    Initial commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,303 @@
+import "./LandingPage.css";
+import LiveAuctions from "./LiveAuctions/LiveAuctions";
+function LandingPage() {
+  return (
+    <div>
+      {/* ================= NAVBAR ================= */}
+      <header className="navbar">
+        
+        <div className="nav-left">
+  <img src="/logo.png" alt="Logo" className="logo" />
+</div>
+        <nav className="nav-center">
+          <a href="#home">Home</a>
+          <a href="#about">Who We Are</a>
+          <a href="#auction">Auction</a>
+          <a href="#whyus">Why Us</a>
+          <a href="#faq">FAQ</a>
+        </nav>
+
+        <div className="nav-right">
+          <button className="cta-btn">Start Bidding Now</button>
+        </div>
+      </header>
+
+      {/* ================= HERO ================= */}
+      <section id="home" className="hero">
+      <img src="/bgi.jpg" className="bg-image"/>
+        <div className="hero-content">
+        <h4 className="head">WHERE EVERY BID WINS TRUST</h4>
+          <h1 className="heading">Where Auctions Are <br/>Transparent and Trusted</h1>
+          <p>
+            A transparent auction platform connecting verified buyers and
+            sellers through real-time, secure bidding.
+          </p>
+          <button className="btn_primary">Get Started for free</button>
+          <button className="btn_secondary">Browse Auctions<span class="arrow"> →</span></button>
+        </div>
+      </section>
+      {/*===================IMAGE SECTION=================*/}
+      <section className="image-section">
+        <img src="/home.png" alt="Showcase" className="between-image"></img>
+      </section>
+      {/* ================= WHO WE ARE ================= */}
+     <section id="about" className="who-we-are">
+  <div className="who-container">
+    {/* LEFT IMAGE */}
+    <div className="who-image">
+      <img src="/whoweare.png" />
+
+      <div className="stat stat-top">
+        <strong>24/7</strong>
+        <span>Live auctions</span>
+      </div>
+
+      <div className="stat stat-bottom">
+        <strong>3.5M+</strong>
+        <span>Listing views</span>
+      </div>
+    </div>
+
+    {/* RIGHT CONTENT */}
+    <div className="who-content">
+      <span className="who-tag">WHO WE ARE</span>
+
+      <h2>Built on trust, powered by Technology</h2>
+
+      <p className="who-desc">
+        We are a secure digital auction platform connecting verified buyers and
+        sellers through transparent, real-time bidding. Our goal is to simplify
+        auctions while maintaining trust, compliance and efficiency for every participant.
+      </p>
+
+      <div className="who-points">
+        <details open>
+          <summary>Verified & Trusted</summary>
+          <p>
+            Every user and listing is verified to ensure safe, genuine, and
+            transparent auctions.
+          </p>
+        </details>
+
+        <details>
+          <summary>Real-Time Transparency</summary>
+          <p>
+            Live bidding updates, full vehicle history, and no hidden fees.
+          </p>
+        </details>
+
+        <details>
+          <summary>Secure End-to-End Platform</summary>
+          <p>
+            From bidding to payments and delivery, everything is protected with
+            enterprise-grade security.
+          </p>
+        </details>
+      </div>
+    </div>
+  </div>
+</section>
+
+{/* ================= TRUSTED AUCTIONS ================= */}
+      <section className="trusted-auctions">
+        <div className="trusted-container">
+          <span className="section-tag">LIVE AUCTIONS</span>
+          <h2 className="trusted-title">Real-Time Auctions You Can Trust</h2>
+          <p className="trusted-subtitle">
+            Bid on verified listings with full visibility and clear pricing.
+          </p>
+          <button className="trusted-btn">Browse Auctions</button>
+        </div>
+        <LiveAuctions/>
+      </section>
+
+
+      {/* ================= VISUAL FEATURES ================= */}
+      <section className="visual-features">
+        <div className="visual-inner">
+
+          <div className="visual-header">
+            <span className="tag">FEATURES</span>
+            <h2>Powerful Features Built for Auctions</h2>
+            <p>
+              Advanced features that ensure fairness, security, and real-time
+              performance
+            </p>
+          </div>
+
+          {/* Feature 1 */}
+          <div className="feature-row">
+            <div className="feature-text">
+              <h3>Built for Serious Dealers</h3>
+              <p>
+                A professional auction experience with verified access,
+                real-time bidding, full vehicle transparency, and a secure
+                post-auction process.
+              </p>
+              <a href="#">Learn more →</a>
+            </div>
+
+            <div className="feature-image">
+              <img
+                src="/f1.png"
+                alt="Auction Dashboard"
+              />
+            </div>
+          </div>
+
+          {/* Feature 2 */}
+          <div className="feature-row reverse">
+            <div className="feature-text">
+              <h3>Professional Auctions, End to End</h3>
+              <p>
+                From registration to delivery, we handle the complete auction
+                lifecycle with secure payments and verified vehicle history.
+              </p>
+              <a href="#">Learn more →</a>
+            </div>
+
+            <div className="feature-image">
+              <img
+                src="/f2.png"
+                alt="Auction Platform"
+              />
+            </div>
+          </div>
+
+          {/* Feature 3 */}
+          <div className="feature-row">
+            <div className="feature-text">
+              <h3>Transparent Vehicle Auctions</h3>
+              <p>
+                No hidden fees, no undisclosed damage. Full inspection reports
+                and detailed photo galleries for every vehicle.
+              </p>
+              <a href="#">Learn more →</a>
+            </div>
+
+            <div className="feature-image">
+              <img
+                src="/f3.png"
+              />
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      {/* ================= WHY US ================= */}
+      <section id="whyus" className="features">
+        <h2>Why AutoBid?</h2>
+
+        <div className="feature-list">
+          <div className="feature-card">
+            <h3>Live Bidding</h3>
+            <p>Participate in real-time auctions with instant updates.</p>
+          </div>
+
+          <div className="feature-card">
+            <h3>Verified Cars</h3>
+            <p>All vehicles are verified and inspected before listing.</p>
+          </div>
+
+          <div className="feature-card">
+            <h3>Secure Payments</h3>
+            <p>Safe and transparent transactions every time.</p>
+          </div>
+        </div>
+      </section>
+
+      {/* ================= FAQ ================= */}
+      <section id="faq" className="faq">
+        <h2>Frequently Asked Questions</h2>
+
+        <details>
+          <summary>How do I create a seller account?</summary>
+          <p>You can sign up and complete the verification process.</p>
+        </details>
+
+        <details>
+          <summary>What are the fees for listing a car?</summary>
+          <p>Fees depend on the auction type and vehicle category.</p>
+        </details>
+
+        <details>
+            <summary>How does the shipping process work?</summary>
+            <p> We partner with certified auto transport companies nationwide. After winning an auction, you can arrange shipping directly through our platform for a smooth and secure delivery experience.</p>
+        </details>
+
+        <details>
+          <summary>Are there any volume discounts for dealers?</summary>
+          <p>Yes. We offer tiered subscription plans for professional dealers who buy or list multiple vehicles each month, helping you save more as your volume grows.</p>
+        </details>
+
+        <details>
+          <summary>What payment methods are supported?</summary>
+          <p>We support secure bank transfers, major credit cards and integrated escrow services to ensure buyer protection and safe transactions</p>
+        </details>
+
+      </section>
+
+      {/* ================= PRE FOOTER ================= */}
+      <section className="pre-footer">
+        <h2>Join a Trusted Dealer Auction Network</h2>
+        <p>Join Ireland's trusted vehicle auctions and unlock exclusive deals</p>
+
+        <div className="pre-footer-buttons">
+          <button className="primary-btn">Get Started Free</button>
+          <button className="secondary-btn">Browse Auctions<span class="arrow"> →</span></button>
+        </div>
+      </section>
+
+      {/* ================= FOOTER ================= */}
+      <footer className="footer">
+  <div className="footer-container">
+    {/* Left brand */}
+    <div className="footer-brand">
+      <h3>AutoBid Pro</h3>
+
+      <div className="social-icons">
+        <span>🐦</span>
+        <span>📷</span>
+        <span>▶️</span>
+        <span>💼</span>
+      </div>
+    </div>
+
+    {/* Columns */}
+    <div className="footer-column">
+      <h4>ABOUT</h4>
+      <a href="#">Overview</a>
+      <a href="#">Features</a>
+      <a href="#">Solutions</a>
+      <a href="#">Tutorials</a>
+      <a href="#">Pricing</a>
+    </div>
+
+    <div className="footer-column">
+      <h4>COMPANY</h4>
+      <a href="#">About Us</a>
+      <a href="#">Careers</a>
+      <a href="#">Press</a>
+      <a href="#">News</a>
+      <a href="#">Contact</a>
+    </div>
+
+    <div className="footer-column">
+      <h4>LEGAL</h4>
+      <a href="#">Terms of Service</a>
+      <a href="#">Privacy Policy</a>
+      <a href="#">Cookie Policy</a>
+      <a href="#">Disclaimer</a>
+    </div>
+  </div>
+
+  <div className="footer-bottom">
+    © 2026 AutoBid Pro. All rights reserved.
+  </div>
+</footer>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/components/LiveAuctions/LiveAuctions.js
@@ -0,0 +1,223 @@
+import "./LiveAuctions.css";
+
+function LiveAuctions() {
+  return (
+    <section className="live-auctions">
+    <div className="live-auctions-inner">
+      <div className="slider-container">
+        <div className="slider-track">
+
+          <div className="auction-card">
+            <h3 className="car-name">Nissan Model:Qashqai</h3>
+            <span className="time">03h 30m</span>
+            <p className="condition">Overall Condition:<span>Fair</span></p>
+            <p className="curbid">Current Bid</p>
+            <p className="bid">€13,000</p>
+            <img src="/car.png" alt="car" />
+            <div class="car-specs">
+    <div class="spec">
+      <p class="spec-label">Odometer</p>
+      <p class="spec-value">80,000 km</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Fuel Type</p>
+      <p class="spec-value">Diesel</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Transmission</p>
+      <p class="spec-value">Automatic</p>
+    </div>
+  </div>
+            <button className="view">View Auction</button>
+          </div>
+
+          <div className="auction-card">
+            <h3 className="car-name">Kia Model:Sportage</h3>
+            <span className="time">03h 30m</span>
+            <p className="condition">Overall Condition:<span>Good</span></p>
+            <p className="curbid">Current Bid</p>
+            <p className="bid">€16,500</p>
+            <img src="/car.png" alt="car"/>
+            <div class="car-specs">
+    <div class="spec">
+      <p class="spec-label">Odometer</p>
+      <p class="spec-value">80,000 km</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Fuel Type</p>
+      <p class="spec-value">Diesel</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Transmission</p>
+      <p class="spec-value">Automatic</p>
+    </div>
+  </div>
+            <button className="view">View Auction</button>
+          </div>
+
+          <div className="auction-card">
+            <h3 className="car-name">Mercedes Model:C-Class</h3>
+            <span className="time">03h 30m</span>
+            <p className="condition">Overall Condition:<span>Fair</span></p>
+            <p className="curbid">Current Bid</p>
+            <p className="bid">€12,500</p>
+            <img src="/car.png" alt="car" />
+            <div class="car-specs">
+    <div class="spec">
+      <p class="spec-label">Odometer</p>
+      <p class="spec-value">80,000 km</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Fuel Type</p>
+      <p class="spec-value">Diesel</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Transmission</p>
+      <p class="spec-value">Automatic</p>
+    </div>
+  </div>
+            <button className="view">View Auction</button>
+          </div>
+
+          <div className="auction-card">
+            <h3 className="car-name">Hyundai Model:i30</h3>
+            <span className="time">03h 30m</span>
+            <p className="condition">Overall Condition:<span>Excellent</span></p>
+            <p className="curbid">Current Bid</p>
+            <p className="bid">€15,000</p>
+            <img src="/car.png" alt="car" />
+            <div class="car-specs">
+    <div class="spec">
+      <p class="spec-label">Odometer</p>
+      <p class="spec-value">80,000 km</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Fuel Type</p>
+      <p class="spec-value">Diesel</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Transmission</p>
+      <p class="spec-value">Automatic</p>
+    </div>
+  </div>
+            <button className="view">View Auction</button>
+          </div>
+          <div className="auction-card">
+            <h3 className="car-name">Nissan Model:Qashqai</h3>
+            <span className="time">03h 30m</span>
+            <p className="condition">Overall Condition:<span>Fair</span></p>
+            <p className="curbid">Current Bid</p>
+            <p className="bid">€13,000</p>
+            <img src="/car.png" alt="car" />
+            <div class="car-specs">
+    <div class="spec">
+      <p class="spec-label">Odometer</p>
+      <p class="spec-value">80,000 km</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Fuel Type</p>
+      <p class="spec-value">Diesel</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Transmission</p>
+      <p class="spec-value">Automatic</p>
+    </div>
+  </div>
+            <button className="view">View Auction</button>
+          </div>
+
+          <div className="auction-card">
+            <h3 className="car-name">Kia Model:Sportage</h3>
+            <span className="time">03h 30m</span>
+            <p className="condition">Overall Condition:<span>Good</span></p>
+            <p className="curbid">Current Bid</p>
+            <p className="bid">€16,500</p>
+            <img src="/car.png" alt="car" />
+            <div class="car-specs">
+    <div class="spec">
+      <p class="spec-label">Odometer</p>
+      <p class="spec-value">80,000 km</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Fuel Type</p>
+      <p class="spec-value">Diesel</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Transmission</p>
+      <p class="spec-value">Automatic</p>
+    </div>
+  </div>
+            <button className="view">View Auction</button>
+          </div>
+
+          <div className="auction-card">
+            <h3 className="car-name">Mercedes Model:C-Class</h3>
+            <span className="time">03h 30m</span>
+            <p className="condition">Overall Condition:<span>Fair</span></p>
+            <h4 className="curbid">Current Bid</h4>
+            <p className="bid">€12,500</p>
+            <img src="/car.png" alt="car" />
+            <div class="car-specs">
+    <div class="spec">
+      <p class="spec-label">Odometer</p>
+      <p class="spec-value">80,000 km</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Fuel Type</p>
+      <p class="spec-value">Diesel</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Transmission</p>
+      <p class="spec-value">Automatic</p>
+    </div>
+  </div>
+            <button className="view">View Auction</button>
+          </div>
+          <div className="auction-card">
+            <h3 className="car-name">Hyundai Model:i30</h3>
+            <span className="time">03h 30m</span>
+            <p className="condition">Overall Condition:<span>Excellent</span></p>
+            <h4 className="curbid">Current Bid</h4>
+            <p className="bid">€15,000</p>
+            <img src="/car.png" alt="car" />
+            <div class="car-specs">
+    <div class="spec">
+      <p class="spec-label">Odometer</p>
+      <p class="spec-value">80,000 km</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Fuel Type</p>
+      <p class="spec-value">Diesel</p>
+    </div>
+
+    <div class="spec">
+      <p class="spec-label">Transmission</p>
+      <p class="spec-value">Automatic</p>
+    </div>
+  </div>
+            <button className="view">View Auction</button>
+          </div>
+
+        </div>
+      </div>
+      </div>
+    </section>
+  );
+}
+
+export default LiveAuctions;
